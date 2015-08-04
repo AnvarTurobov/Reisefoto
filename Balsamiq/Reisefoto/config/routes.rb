ea34b9f022bd5f60af1660269dc88de5fdc6db46
AnvarTurobov <anvar.turobov@gmail.com>
2015-08-04 09:13:18 +0100

    set up a static page

--- Balsamiq/Reisefoto/config/routes.rb
+++ Balsamiq/Reisefoto/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
-  root "users#index"
   resources :locations
+  root "pages_controller#home"
+  resources :sessions, only:[:new, :create, :destroy]
+  resources :photos
+  resources :users 
+  get "login", to: "users#new"
+#get "login", to: "sessions#new"
+  delete "logout", to: "sessions#destroy"
 end
