a4efd2cf8a011980b0e1a899502f031b9d116823
AnvarTurobov <anvar.turobov@gmail.com>
2015-08-06 16:22:57 +0100

    changed the styling with foundation

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -3,4 +3,10 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  
+########
+  version :thumb do
+      process :resize_to_fill => [100, 100]
+    end
+
 end
